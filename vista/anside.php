f8f34a6a81d0fc01407d6698ca0be7de29837517
carlosgiron <carlosgiron.2092@gmail.com>
2016-11-26 13:20:02 -0500

    .

--- vista/anside.php
+++ vista/anside.php
@@ -1,22 +1,22 @@
 <div class="col-xs-hidden col-sm-15 col-md-15 col-lg-15">
     <div class="img-anside">
-        <img class="img-responsive" src="http://lorempixel.com/500/500/animals"/> 
+        <img class="img-responsive" src="http://lorempixel.com/400/400/animals"/> 
         
     </div>
      <div class="img-anside">
-        <img class="img-responsive" src="http://lorempixel.com/500/500/food"/> 
+        <img class="img-responsive" src="http://lorempixel.com/400/400/food"/> 
         
     </div>
      <div class="img-anside">
-        <img class="img-responsive" src="http://lorempixel.com/500/500/sports"/> 
+        <img class="img-responsive" src="http://lorempixel.com/400/400/sports"/> 
         
     </div>
      <div class="img-anside">
-        <img class="img-responsive" src="http://lorempixel.com/500/500/nightlife"/> 
+        <img class="img-responsive" src="http://lorempixel.com/400/400/nightlife"/> 
         
     </div>
      <div class="img-anside">
-        <img class="img-responsive" src="http://lorempixel.com/500/500/"/> 
+        <img class="img-responsive" src="http://lorempixel.com/400/400/"/> 
         
     </div>
 </div>
--- vista/header.php
+++ vista/header.php
@@ -1,8 +1,98 @@
 <header>
+    <div class="container-fluid back-top" >
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-60 col-sm-60 col-md-60 col-lg-60 text-center">
+                        <div class="square-top"></div>
+
+                        <strong class=" transmicion-01 text-uppercase">transmición en vivo</strong>
+                    </div> 
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid back-top-02" >
+        <div class="row">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-xs-60 col-sm-30 col-md-20 col-lg-20"> 
+                        <?php
+                        date_default_timezone_set("America/Lima");
+//                        $fechaactual = date();
+//                        print_r($fechaactual);
+                        $dias = array("Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado");
+                        $mes = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+
+                        echo "<span class='get-date'>" . $dias[date('w')] . " " . date("d") . " de " . $mes[date('m') - 1] . " del " . date("Y") . "</span>";
+                        ?>
+                    </div>
+                    <div class="col-xs-60 col-sm-30 col-md-20 col-lg-20 social-top"> 
+                        <span>Siguenos en: </span>
+                        <a href="#"><span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                            </span></a>
+                        <a href="#"><span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                            </span></a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
     <div class="container">
         <div class="row">
-        <div class="navbar navbar-default">
+            <div class="col-xs-60 col-sm-30 col-md-30 col-lg-30 ">
+                <img class="img-responsive" src="img/noticia-educa.jpg" alt="noticia-educa"/>
 
+            </div>
+            <div class="col-xs-60 col-sm-30 col-md-30 col-lg-30 ">
+
+                <div class="reportaje">
+                    <div class="row">
+                        <div class="col-xs-60 col-sm-30 col-md-30 col-lg-30 repor-01">
+                            <span >Reportajes</span>
+                            <ul>
+                                <li>Profesores</li>
+                                <li>Alumnos</li>
+                                <li>Clases de Éxito</li>
+
+                            </ul>
+                        </div>
+
+                        <div class="col-xs-60 col-sm-30 col-md-30 col-lg-30 forma-01">
+                            <span>Formación</span>
+                            <ul>
+                                <li>Libros y materiales de estudio</li>
+                                <li>Consejos</li>
+                                <li>Guías prácticas</li>
+
+                            </ul>
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <div class="navbar navbar-default">
+        <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -29,16 +119,17 @@
             </div>
 
         </div>
-        <div class="menu-sec col-xs-60 col-sm-60 col-md-60 col-lg-60">
-            <button class="btn btn-default">Tu Opinión importa</button>
-            <button class="btn btn-default">ADN del estudios</button>
-            <button class="btn btn-default">El rincón del maestro</button>
-            <button class="btn btn-default">YouTube</button>
-            <button class="btn btn-default">Facebook</button>
 
-        </div>
     </div>
-     </div>
+    <div class="menu-sec col-xs-60 col-sm-60 col-md-60 col-lg-60">
+        <button class="btn btn-default">Tu Opinión importa</button>
+        <button class="btn btn-default">ADN del estudios</button>
+        <button class="btn btn-default">El rincón del maestro</button>
+        <button class="btn btn-default">YouTube</button>
+        <button class="btn btn-default">Facebook</button>
+
+    </div>
+
 </header>
 
 
